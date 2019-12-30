27a2ccf5ae4be7565ae5b69086876bdec4ffb87f
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-30 01:43:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,13 +4,9 @@ class Shoe
 
   BRANDS = []
 
-  def brand= (brand)
-    @brand = brand 
-    BRANDS << brand unless BRANDS.include?(brand) 
-  end 
-
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand) 
   end
 
   def cobble
